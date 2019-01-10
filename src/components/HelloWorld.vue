325bb16eb599a5a3eccd432eadc1ff91f96d583c
Michael Solati <mkslt04@gmail.com>
2019-01-10 13:21:29 -0800

    chore: install nge-slides

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,23 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <nge-slides-wrapper>
+    <nge-slide-title title="Angular Element Slides" subtitle="Web Components Made Easy"></nge-slide-title>
+    <nge-slide-section title="Section Slide"></nge-slide-section>
+    <nge-slide title="Slide 3" content="Content can be defined via an attribute"></nge-slide>
+    <nge-slide title="Slide 4">
+      <p>Or you can put your content in as HTML</p>
+    </nge-slide>
+    <nge-slide-blank>
+      <p>A blank slide has little to no properties</p>
+      <p>All it's designed to be is an HTML passthrough</p>
+    </nge-slide-blank>
+    <nge-slide-title title="Slide 6"></nge-slide-title>
+    <nge-slides-progress-bar></nge-slides-progress-bar>
+  </nge-slides-wrapper>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
-}
+export default class HelloWorld extends Vue {}
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
+import 'nge-slides';
 
 Vue.config.productionTip = false;
+
+Vue.config.ignoredElements = [
+  'nge-slides-wrapper',
+  'nge-slide-title',
+  'nge-slide-section',
+  'nge-slide',
+  'nge-slide-blank',
+  'nge-slides-progress-bar',
+];
 
 new Vue({
   render: (h) => h(App),
